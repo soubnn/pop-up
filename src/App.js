701b422d1b1dbce95fc191df9b2b1afebe34eb5f
soubnn <soubnn19@gmail.com>
2023-12-18 11:27:21 +0530

    new commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,84 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
-function App() {
+const App = () => {
+  const [showPopup, setShowPopup] = useState(false);
+  const [formData, setFormData] = useState({
+    username: '',
+    email: '',
+    password: '',
+  });
+
+  const togglePopup = () => {
+    setShowPopup(!showPopup);
+  };
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({
+      ...formData,
+      [name]: value,
+    });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log('Form submitted:', formData);
+    setFormData({
+      username: '',
+      email: '',
+      password: '',
+    });
+    togglePopup();
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <h1>Create new account!</h1>
+      <button style={{ backgroundColor: '#4caf50', }} onClick={togglePopup}>Click</button>
+      {showPopup && (
+        <div className="popup">
+          <div className="popup-inner">
+            <h2>Sign Up</h2>
+            <form style={{ marginRight: '15px' }} onSubmit={handleSubmit}>
+
+              <label>
+                Username:
+                <input
+                  type="text"
+                  name="username"
+                  value={formData.username}
+                  onChange={handleChange}
+                />
+              </label>
+              <label>
+                Email:
+                <input
+                  type="email"
+                  name="email"
+                  value={formData.email}
+                  onChange={handleChange}
+                />
+              </label>
+              <label>
+                Password:
+                <input
+                  type="password"
+                  name="password"
+                  value={formData.password}
+                  onChange={handleChange}
+                />
+              </label>
+              <button className='b-bttn' style={{ marginLeft: '100px', marginBottom: '5px' }}
+                type="submit">Sign Up</button>
+            </form>
+            <button className='b-bttn' style={{ backgroundColor: '#ce1e1e' }} onClick={togglePopup}>Close</button>
+          </div>
+        </div>
+      )
+      }
+    </div >
   );
-}
+};
 
 export default App;
